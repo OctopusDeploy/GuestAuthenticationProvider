a1c61725ac58696d3ee0180f7194e9de5908ae63
Shannon Lewis <slewis74@gmail.com>
2019-06-25 20:20:27 +1000

    fix AssemblyInfo setup that was preventing version number being assigned to the dll during the build

--- source/Server/Properties/AssemblyInfo.cs
+++ source/Server/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Octopus.Server.Extensibility.Guest")]
-
 [assembly: Guid("f284f2cc-60a4-4961-aefa-dacf14616c4f")]
